6ea7a87b76951718d98c90e29e15027884e41616
Lauro Antonio Galaviz Castro <galavizcastro@gmail.com>
2014-11-24 16:01:07 -0600

    application

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Verdemonarcaonrails
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.assets.enabled = true
     config.serve_static_assets = true
     config.assets.precompile += %w( *.css, *.js )
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
