c4d1c9e0724e12e2427e3a87ce490bdf096faa24
Lauro Antonio Galaviz Castro <galavizcastro@gmail.com>
2014-11-25 12:03:49 -0600

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -27,12 +28,12 @@ ActiveRecord::Schema.define(version: 20141112135510) do
     t.string   "disponibilidad"
     t.datetime "created_at"
     t.datetime "updated_at"
-
+  end
 
   create_table "projects", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
